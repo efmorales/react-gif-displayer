ba2aa897d8e52e4c5003b16b25400df590fb4196
efmorales <54375116+efmorales@users.noreply.github.com>
2024-03-07 14:28:39 -0500

    updated GifGrid.test.jsx with relevant tests for the component

--- tests/components/GifGrid.test.jsx
+++ tests/components/GifGrid.test.jsx
@@ -1,5 +1,50 @@
-import {render, screen} from '@testing-library/react';
-import {GifGrid} from '../../src/components/GifGrid';
+import { render, screen } from '@testing-library/react';
+import { GifGrid } from '../../src/components/GifGrid';
+import { useFetchGifs } from '../../src/hooks/useFetchGifs';
+
+jest.mock('../../src/hooks/useFetchGifs');
 
 describe('GifGrid component testing', () => {
-});
+
+    const category = 'One Punch';
+
+    test('should show the loading message', () => {
+
+        useFetchGifs.mockReturnValue({
+            images: [],
+            isLoading: true
+        });
+
+        render(<GifGrid category={category} />);
+        expect(screen.getByText('Loading...')).toBeTruthy();
+
+    });
+
+    test('should show the items when images are loaded', () => {
+
+        const gifs = [
+            {
+                id: 'ABC',
+                title: 'Any title',
+                url: 'https://localhost/any.jpg'
+            },
+            {
+                id: 'DEF',
+                title: 'Any title',
+                url: 'https://localhost/any2.jpg'
+            }
+        ];
+        
+        useFetchGifs.mockReturnValue({
+            images: gifs,
+            isLoading: false
+        });
+
+        render(<GifGrid category={category} />);
+
+        expect(screen.getAllByRole('img').length).toBe(2);
+
+
+    });
+
+})
